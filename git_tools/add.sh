fd29d41ad50459e05e7092850a79ca574b326b39
Julian Vanecek <jsv@cs.umd.edu>
2018-10-30 17:28:42 -0400

    fixed_the_scripts

--- git_tools/add.sh
+++ git_tools/add.sh
@@ -1,7 +1,10 @@
-if $# != 2; then
-	echo "missing arg1_file arg2_message"
+#!/bin/bash
+if (($# != 2)); then
+	echo "missing [arg1_file arg2_message_no_spaces!], this will ADD the items"
+else
+	echo "adding to git origin"
+	git pull
+	git add $1
+	git commit -m $2
+	git push
 fi
-git pull
-git add $1
-git commit -m $2
-git push
--- git_tools/rm.sh
+++ git_tools/rm.sh
@@ -1,7 +1,10 @@
-if $# != 2; then
-	echo "missing arg1_file arg2_message"
+#!/bin/bash
+if (($# != 2)); then
+	echo "missing [arg1_file arg2_message_no_spaces!]. This will REMOVE the given arg1"
+else
+	echo "removing from git origin"
+	git pull
+	git rm $1
+	git commit -m $2
+	git push
 fi
-git pull
-git rm $1
-git commit -m $2
-git push
