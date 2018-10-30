7c3c87237fcea90abc8f4e4367e42e4490b95c6b
Omer Akgul <omerakgul@wireless-10-104-173-221.umd.edu>
2018-10-30 18:20:39 -0400

    scraping

--- avalon_scraping/scrape.py
+++ avalon_scraping/scrape.py
@@ -1,16 +1,40 @@
 import requests
 from bs4 import BeautifulSoup
 
-page = requests.get('http://avalon.law.yale.edu/subject_menus/17th.asp')
 
-soup = BeautifulSoup(page.text, 'html.parser')
-links = soup.find_all('a')
+def not_in_filter(link):
+    filter = ['http', 'default', '.pdf', '21st', '#', '20th_century']
+    for word in filter:
+        if link.find(word) > -1:
+            return False
 
-for link in links:
-    print(link.get('href'))
+    return True
 
-    page = requests.get('http://avalon.law.yale.edu/subject_menus/' + link.get('href'))
+
+def scrape(init_link, visited):
+    page = requests.get(init_link)
 
     content = BeautifulSoup(page.text, 'html.parser')
+
     if content.p:
-        print(content.p)
+        print(init_link)
+        #print(content.p)
+    else:
+        links = content.find_all('a')
+        for link in links:
+            link = link.get('href')
+            if link and not_in_filter(link):
+
+                link = 'http://avalon.law.yale.edu/subject_menus/' + link
+                if link.find('..') > -1 and  link not in visited:
+
+                    visited.add(link)
+                    #print(link)
+                    scrape(link, visited)
+
+
+
+
+if __name__ == '__main__':
+    visited = set()
+    scrape('http://avalon.law.yale.edu/subject_menus/15th.asp', visited)
