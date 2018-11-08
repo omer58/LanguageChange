da1f0d160f23fd124e348740ead671e38e105790
Julian Vanecek <jsv@cs.umd.edu>
2018-11-08 18:37:06 -0500

    updatedYearVectorizor

--- preprocess/langmodels/word2vec.py
+++ preprocess/langmodels/word2vec.py
@@ -1,1 +1,20 @@
-word2vec file shojuld go here for use in building models
+from collections import defaultdict
+
+word2YearVec = defaultdict(defaultdict)
+totalWordCounts = defaultdict(int)
+
+#make word vectors
+for doc, year in input_data:
+    for word in doc:
+        word2YearVec[word][year] += 1
+        totalWordCounts[word += 1
+
+
+
+#scale word vectors, divide each word in year, by total occurance count of that word. 
+for word, totalCount in totalWordCounts.items():
+    for year, yearCount in word2YearVec[word].items():
+        word2YearVec[word][year] = yearCount / totalCount
+    
+
+
