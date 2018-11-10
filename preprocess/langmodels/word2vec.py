af5d6a3a49802dc68168df44d929d93e9d77d19c
Julian Vanecek <jsv@cs.umd.edu>
2018-11-10 17:48:58 -0500

    fixed_word2vec.py

--- preprocess/langmodels/word2vec.py
+++ preprocess/langmodels/word2vec.py
@@ -1,20 +1,23 @@
 from collections import defaultdict
 
 word2YearVec = defaultdict(defaultdict)
-totalWordCounts = defaultdict(int)
+totalYearCounts = defaultdict(int)
 
 #make word vectors
 for doc, year in input_data:
     for word in doc:
         word2YearVec[word][year] += 1
-        totalWordCounts[word += 1
+        totalYearCounts[year] += 1
 
-
+totalYears = len(totalYearCounts.keys())
 
 #scale word vectors, divide each word in year, by total occurance count of that word. 
-for word, totalCount in totalWordCounts.items():
-    for year, yearCount in word2YearVec[word].items():
-        word2YearVec[word][year] = yearCount / totalCount
-    
+for word, wordVec in word2YearVec.items():
+    yearsWithWord = len(wordVec)
 
+    for year in wordVec.keys():
+        word2YearVec[word][year] = (wordVec[year] / totalYearCounts[year]) \
+                                    * (totalYears / yearsWithWord)
+
+return shit
 
