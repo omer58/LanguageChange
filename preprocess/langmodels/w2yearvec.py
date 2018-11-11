aa55b5e3eb3c1a3057d4f13c0f5f7b01ede0a267
Julian Vanecek <jsv@cs.umd.edu>
2018-11-11 15:57:45 -0500

    finished_year_vec_not_tested

--- preprocess/langmodels/w2yearvec.py
+++ preprocess/langmodels/w2yearvec.py
@@ -1,23 +1,30 @@
 from collections import defaultdict
+import pickle
 
+year_book = pickle.loads('../../data_sets/year_book.pickle')
 word2YearVec = defaultdict(defaultdict)
 totalYearCounts = defaultdict(int)
+wordsInYear = defaultdict(set())
 
 #make word vectors
-for doc, year in input_data:
-    for word in doc:
-        word2YearVec[word][year] += 1
-        totalYearCounts[year] += 1
+for i, documents in enumerate(year_book):
+    year = i+1000
+    for doc in documents:
+        for word in doc:
+            word2YearVec[word][year] += 1
+            totalYearCounts[year] += 1
+            wordsInyear[word].add(year)
 
-totalYears = len(totalYearCounts.keys())
+totalYears = len(year_book)
 
 #scale word vectors, divide each word in year, by total occurance count of that word. 
 for word, wordVec in word2YearVec.items():
-    yearsWithWord = len(wordVec)
+    yearsWithWord = len(wordsInYear[word])
 
     for year in wordVec.keys():
         word2YearVec[word][year] = (wordVec[year] / totalYearCounts[year]) \
                                     * (totalYears / yearsWithWord)
 
-return shit
+
+pickle.dump(word2YearVec, open('../../data_sets/w2yv.p', 'wb'))
 
