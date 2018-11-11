775fced551b3ba88ceb3efa050d931fe964f9a28
Julian Vanecek <jsv@cs.umd.edu>
2018-11-11 18:31:08 -0500

    finilized

--- preprocess/model_factory/TokenCleaner.py
+++ preprocess/model_factory/TokenCleaner.py
@@ -5,24 +5,15 @@ from nltk.tokenize import TreebankWordTokenizer
 class Cleaner:
     def __init__():
         self.stopwords   = set(stopwords.words('english')).union(['\''])
-        self.digits      = set(str(i) for i in range(10)))
-        self.t           = TreebankWordTokenizer().tokenize
 
-    def tokenize(self, x):
-        return self.t(x)
-
-    def assess_indiv(self, x):
+    def _assess_indiv_(self, x):
         if len(x)>50:
             return False
         if x in self.stopwords:
-            return False
-        for xi in x:
-            if xi in self.digits:
-                return False
+            return False      
         return str.lower(x.strip(punctuation))
 
-    def clean(self, x):
-        return [self.asses_indiv(xi) for xi in x]
+    def clean(self, sentence):
+        sent = [self.asses_indiv(xi) for xi in sentence]
+        return [x for x in sent if x]
 
-    def tokenize_clean(self, sentence):
-        return self.assess(self.t(x))
