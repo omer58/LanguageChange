547f08337491c975afa77b9b8a35ef3b5412171f
omer58 <omerakgul58@gmail.com>
2018-11-11 21:24:48 -0500

    bug fixes

--- oracle/year_guesser.py
+++ oracle/year_guesser.py
@@ -1,24 +1,35 @@
 #pseudo code, really
 import pickle
 import numpy as np
+from TokenCleaner import Cleaner
 
-def class Year_Guesser:
+class Year_Guesser:
     def avg_year(self, ques_vec):
         tot = sum(ques_vec)
         ques_vec = [x/tot for x in ques_vec]
         s = 0
-        for i, j in enumerate(l):
+        for i, j in enumerate(ques_vec):
             s += i*j
         return round(s)
 
     def __init__(self):
-        self.year_vecs = pickle.load('../data_sets/y2wv.p')
-    
-    
+        self.year_vecs = pickle.load(open('../../data_sets/w2yv_sample.pickle', 'rb'))
+
+
     def guess(self,question):
-        question_vec =[0.0]*2018
+        question_vec =[0.0]*1019
         for word in question:
-            question_vec = np.add(question_vec, self.year_vecs[word])
-        return self.avg_year(question_vec)
+            if word in self.year_vecs:
+                question_vec = np.add(question_vec, self.year_vecs[word])
+        return self.avg_year(question_vec)+1000
 
 
+import json
+
+ds = json.loads(open('../../qanta-codalab/data/qanta.dev.2018.04.18.json').read())
+clean = Cleaner()
+for iii in range(10):
+    print(ds['questions'][iii])
+    q_t = clean.clean(ds['questions'][iii]['text'])
+    oracle = Year_Guesser()
+    print(oracle.guess(q_t))
