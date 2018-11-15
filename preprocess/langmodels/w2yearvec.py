1e0178d78641b408a7fcb1d0764a45bdcb5ebde8
Julian Vanecek <jsv@cs.umd.edu>
2018-11-14 19:27:33 -0500

    new_year_vectorizor_with_de_noising

--- preprocess/langmodels/w2yearvec.py
+++ preprocess/langmodels/w2yearvec.py
@@ -3,27 +3,44 @@ import pickle
 from math import log
 
 LEN_YEAR_VECTOR = 1019
-year_book = pickle.load(open('../../data_sets/year_book.pickle', 'rb'))
+year_book = pickle.load(open('../../data_sets/year_book_year_dict.pickle', 'rb'))
 word2YearVec = defaultdict(list)            # { word -> LIST(year_i -> REAL) }
 totalYearCounts = defaultdict(int)          # { year -> int_tot_wrd_count }
-wordsInYear = defaultdict(set)            # { word -> SET(year0, year1, year2) }
+wordsInYear = defaultdict(set)              # { word -> SET(year0, year1, year2) }
 
-#make word vectors
-for year, documents in enumerate(year_book):
-    for doc in documents:
-        for word in doc:
-            try:
-                word2YearVec[word][year] += 1
-            except IndexError:
-                for i in range(LEN_YEAR_VECTOR):
-                    word2YearVec[word].append(0.0)
-                word2YearVec[word][year] += 1
-            totalYearCounts[year] += 1
-            wordsInYear[word].add(year)
+for year, year_page in enumerate(year_book):
+    for word, count in year_page.items():
+        try:
+            word2YearVec[word][year] = count
+        except IndexError:
+            for i in range(LEN_YEAR_VECTOR):
+                word2YearVec[word].append(0.0)
+            word2YearVec[word][year] = count
+        totalYearCounts[year] += count
+        wordsInYear[word].add(year)
 
-totalYears = len(year_book)
+word2YearVec = dict(word2YearVec)
+
+print('denoising...')
+WINDOW_SIZE = 5;
+half_window = int( WINDOW_SIZE/2)
+for word in word2YearVec:
+    for i in range(len(word2YearVec[word])):
+        if i - half_window > -1 and i+half_window < len(word2YearVec[word]):
+            if sum(word2YearVec[word][i - half_window : i + half_window +1]) == word2YearVec[word][i]:
+                word2YearVec[word][i] = 0
+
+        elif i - WINDOW_SIZE > -1:
+            if sum(word2YearVec[word][i-WINDOW_SIZE:i]) == word2YearVec[word][i]:
+                word2YearVec[word][i] = 0
+
+        elif i+ WINDOW_SIZE <= len(word2YearVec[word]):
+            if sum(word2YearVec[word][i:i+WINDOW_SIZE]) == word2YearVec[word][i]:
+                word2YearVec[word][i] = 0
+
 
 #scale word vectors, divide each word in year, by total occurance count of that word.
+print('tf-idfing...')
 for word, wordVec in word2YearVec.items():
     yearsWithWord = len(wordsInYear[word])
 
@@ -31,7 +48,7 @@ for word, wordVec in word2YearVec.items():
         if value:
             try:
                 word2YearVec[word][year] = (value / totalYearCounts[year]) \
-                                        * log(totalYears / yearsWithWord)
+                                        * log(LEN_YEAR_VECTOR / yearsWithWord)
 
             except ZeroDivisionError:
                 print(word)
@@ -42,4 +59,7 @@ for word, wordVec in word2YearVec.items():
                 print()
 
 
+
+
+print('pickling...')
 pickle.dump(word2YearVec, open('../../data_sets/w2yv.pickle', 'wb'))
