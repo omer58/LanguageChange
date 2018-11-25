c99322d3d13bc75ef7ec83977d49f368712e33df
Rahil <rahil.parikh@gmail.com>
2018-11-25 00:30:52 -0600

    weighted median

--- oracle/year_guesser.py
+++ oracle/year_guesser.py
@@ -13,8 +13,27 @@ class Year_Guesser:
         ques_vec = [x/tot for x in ques_vec]
         s = 0
         for i, j in enumerate(ques_vec):
-            s += i*j
+            s += i*j 
         return round(s)
+
+
+
+    def weighted_median(ques_vec):
+        total=sum(ques_vec)
+        ques_vec=[x/total for x in ques_vec]  #sum of weights is one
+        #print (ques_vec)
+        total=sum(ques_vec)
+
+        fulcrum=total/2 #(should be 0.5)
+        left_weight=0
+        for i, x in enumerate(ques_vec):
+            left_weight=left_weight+x
+            #print (x)
+            #print (left_weight)
+            if left_weight>fulcrum:
+                return i
+            
+
 
     def __init__(self):
         self.year_vecs = pickle.load(open('../data_sets/w2yv_sample.pickle', 'rb'))
