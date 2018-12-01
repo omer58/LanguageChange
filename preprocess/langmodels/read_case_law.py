fda55ea556f0285e0cc6e94a7bcbc2fba3356f6c
Julian Vanecek <jsv@cs.umd.edu>
2018-12-01 13:59:44 -0500

    goodest

--- preprocess/langmodels/read_case_law.py
+++ preprocess/langmodels/read_case_law.py
@@ -4,59 +4,59 @@ import sys
 import time
 import pickle
 import numpy as np
-start = time.time()
 
 
-data = open('/Users/omerakgul/Downloads/Pennsylvania-20180904-text/data/data.jsonl', 'r')
+def run(file_path='/Users/omerakgul/Downloads/Pennsylvania-20180904-text/data/data.jsonl'):
+    start = time.time()
+    data = open(file_path, 'r')
+    bucket18 = []
+    bucket19 = []
+    count18 = 0
+    count19 = 0
 
-bucket18 = []
-bucket19 = []
+    for idx, i in enumerate(data):
 
-for idx, i in enumerate(data):
-
-    if idx % 1000 == 0:
-        sys.stdout.write("pre progress %d%%   \r" % (idx/213000*100) )
-        sys.stdout.flush()
-
-    if idx > 4000:
-        break;
-
-
-
-    case = json.loads(i)
-    year = int(case['decision_date'][:4])
-    text = []
-    for op in case['casebody']['data']['opinions']:
-        for sentence in op['text'].split('. '):
-            text.append( gensim.utils.simple_preprocess(sentence))
-
-
-
-
-
-    if year > 1916:
-        bucket19 += text
-    elif year > 1816:
-        bucket18 += text
-
-
-def printer(ink):
-    len_ink = len(ink)
-    for i, el in enumerate(ink):
-        if i % 1000 == 0:
-            sys.stdout.write("train progress %d%%   \r" % (idx/len_ink*100) )
+        if idx % 1000 == 0:
+            sys.stdout.write("pre progress %d%%   \r" % (idx/213000*100) )
             sys.stdout.flush()
 
-        yield el
+        if count18>5000 and count19>5000:
+            break
 
-print('saving')
+        case = json.loads(i)
+        year = int(case['decision_date'][:4])
 
-np.save('train_data_19_np', np.array(bucket19))
-np.save('train_data_18_np', np.array(bucket18))
+        if year > 1916 and count19 > 5000:
+            continue
+        if year > 1816 and count18 > 5000:
+            continue
+        if year < 1816:
+            continue
+
+        text = []
+        for op in case['casebody']['data']['opinions']:
+            for sentence in op['text'].split('. '):
+                text.append( gensim.utils.simple_preprocess(sentence))
+
+        if year > 1916:
+            bucket19 += text
+            count19 +=1
+        elif year > 1816:
+            bucket18 += text
+            count18 +=1
 
 
+    def printer(ink):
+        len_ink = len(ink)
+        for i, el in enumerate(ink):
+            if i % 1000 == 0:
+                sys.stdout.write("train progress %d%%   \r" % (idx/len_ink*100) )
+                sys.stdout.flush()
 
-print('saved.')
+            yield el
 
-
-print("time: ", time.time() - start)
+    print('saving')
+    np.save('train_data_19_np', np.array(bucket19))
+    np.save('train_data_18_np', np.array(bucket18))
+    print('saved')
+    print("time: ", time.time() - start)
