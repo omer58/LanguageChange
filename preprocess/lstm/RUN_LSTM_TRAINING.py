a6881a9dc9b20bf72642fb6b8fe98d83bb828f86
Julian Vanecek <julian.vanecek@gmail.com>
2018-12-06 00:31:05 -0500

    WORKING_ON_ADDING_BATCH_SIZE____OTHERWISE_WORKS_BUT_SLOW

--- preprocess/lstm/RUN_LSTM_TRAINING.py
+++ preprocess/lstm/RUN_LSTM_TRAINING.py
@@ -11,13 +11,14 @@ from torch.autograd import Variable
 import sys
 sys.path.append('../model_factory')
 import TokenCleaner
+from torch.utils.data import DataLoader
 
 
 DEFAULT_Q_FILE_PATH = '../../../../qanta-codalab/data/qanta.train.2018.04.18.json'
 DEFAULT_Q_YEAR_PATH = '../../data_sets/wiki_article_to_year.pickle'
 DEFAULT_W2YV_PATH   = '../../data_sets/w2yv.pickle'
 DEFAULT_V_FILE_PATH = '../../../../qanta-codalab/data/qanta.test.2018.04.18.json'
-
+BATCH_SIZE = 32
 
 
 
@@ -40,9 +41,9 @@ class LSTM_Loader:
             self.lstm = torch.load('../../models/'+name)
         else:
             print('loading year dict', self.TIME())
-            w2yv_dict = pickle.load(open(self.w2yv_path, 'rb'))
+            self.w2yv_dict = pickle.load(open(self.w2yv_path, 'rb'))
             print('initializing model', self.TIME())
-            self.lstm           = lstm_model.YearLSTM(w2yv_dict )
+            self.lstm           = lstm_model.YearLSTM(w2yv_dict, BATCH_SIZE )
             self.loss_function  = nn.NLLLoss()
             self.optimizer      = optim.SGD(self.lstm.parameters(), lr=0.003, nesterov=True, momentum=0.9)
             print('preparing train structures', self.TIME())
@@ -71,41 +72,40 @@ class LSTM_Loader:
     def _train_all_epochs_(self, training_data, validation=[], num_epochs=50):
         print('training', self.TIME())
         train_accuracy, train_loss, test_accuracy, test_loss = [], [], [], []
-        print('Epoch 0', end='')
+        print('Epoch 0')#, end='')
+        len_data = len(training_data)
         for epoch in range(num_epochs):
             epoch_correct, epoch_loss, valid_correct, valid_loss = 0.0, 0.0, 0.0, 0.0
-            for sentence, tag in training_data:
+            for iii, (sentence, tag) in enumerate(training_data):
+                print(str(iii), str(len_data), '\r',end='')
                 tag = tag-1000
                 self.lstm.zero_grad()
                 self.lstm.hidden = self.lstm.init_hidden()
-                print('YEAR', tag)
-                target = Variable(torch.tensor([tag]))
-                print(target)
-                pred_year_softmax = self.lstm(sentence)
-                pred_year = torch.argmax(pred_year_softmax)
-                print(pred_year)
-                print(pred_year_softmax)
+                target = Variable(torch.LongTensor([tag]))
+                pred_year = self.lstm(sentence)
                 loss = self.loss_function(pred_year, target)
                 loss.backward()
-                optimizer.step()
+                self.optimizer.step()
                 epoch_loss += loss
-                if pred_year == target:
+                if torch.argmax(pred_year) == target:
                     epoch_correct +=1
             train_accuracy.append(epoch_correct)
             train_loss.append(epoch_loss)
 
             if validation:
                 with torch.no_grad():
-                    for example_X, example_Y in validation:
-                        pred_year  = self.lstm(inputs)
-                        target = Variable(torch.tensor(example_Y))
-                        v_loss = loss_function(pred_year, target)
-                        if pred_year == target:
+                    for iii, (sentence, tag) in enumerate(validation):
+                        tag = tag-1000
+                        self.lstm.hidden = self.lstm.init_hidden()
+                        target = Variable(torch.LongTensor([tag]))
+                        pred_year = self.lstm(sentence)
+                        vloss = self.loss_function(pred_year, target)
+                        valid_loss += vloss
+                        if torch.argmax(pred_year) == target:
                             valid_correct +=1
-                        valid_loss += loss
                     test_accuracy.append(valid_correct)
                     test_loss.append(valid_loss)
-        print('Epoch',str(epoch), self.TIME(),'train_accuracy, train_loss, test_accuracy, test_loss', train_accuracy, train_loss, test_accuracy, test_loss, '\r', end='')
+            print('Epoch',str(epoch), self.TIME(),' train_accuracy, train_loss, test_accuracy, test_loss', train_accuracy, train_loss, test_accuracy, test_loss)#, '\r', end='')
         return (train_accuracy, train_loss, test_accuracy, test_loss)
 
     def train(self):
@@ -115,24 +115,27 @@ class LSTM_Loader:
             for thing in F:
                 j = json.loads(thing)['questions']
                 for question_chunk in j:
-                    question = cleaner.clean(question_chunk['text'])
                     wiki_page = question_chunk['page']
                     if wiki_page in self.wiki_year_dict:
                         wiki_year = self.wiki_year_dict[wiki_page]
+                        question_words = cleaner.clean(question_chunk['text'])
+                        question = [self.w2yv_dict[word] for word in question_words if word in self.w2yv_dict]
                         training_set.append((question, wiki_year))
-                    else:
-                        continue #SKIP THIS QUESTION
         with open(self.v_file_path,'r') as F:
             for thing in F:
                 j = json.loads(thing)['questions']
                 for question_chunk in j:
-                    question = question_chunk['text']
                     wiki_page = question_chunk['page']
                     if wiki_page in self.wiki_year_dict:
                         wiki_year = self.wiki_year_dict[wiki_page]
+                        question_words = cleaner.clean(question_chunk['text'])
+                        question = [self.w2yv_dict[word] for word in question_words if word in self.w2yv_dict]
                         validation_set.append((question, wiki_year))
-                    else:
-                        continue #SKIP THIS QUESTION
 
-        results = self._train_all_epochs_(training_set, validation_set)
+        trainloader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True)
+        validloader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=True)
+        results = self._train_all_epochs_(trainloader,validloader)
         self.save_data_model(results, self.lstm)
+
+
+INSTANCE = LSTM_Loader('TEST_1')
