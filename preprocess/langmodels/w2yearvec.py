7f1a035d96fd9ccd511d944cee173b5d7eec3304
omer58 <omerakgul58@gmail.com>
2018-12-05 23:20:39 -0500

    methods to replace pickling

--- preprocess/langmodels/w2yearvec.py
+++ preprocess/langmodels/w2yearvec.py
@@ -2,64 +2,91 @@ from collections import defaultdict
 import pickle
 from math import log
 
-LEN_YEAR_VECTOR = 1019
-year_book = pickle.load(open('../../data_sets/year_book_year_dict.pickle', 'rb'))
-word2YearVec = defaultdict(list)            # { word -> LIST(year_i -> REAL) }
-totalYearCounts = defaultdict(int)          # { year -> int_tot_wrd_count }
-wordsInYear = defaultdict(set)              # { word -> SET(year0, year1, year2) }
-
-for year, year_page in enumerate(year_book):
-    for word, count in year_page.items():
-        try:
-            word2YearVec[word][year] = count
-        except IndexError:
-            for i in range(LEN_YEAR_VECTOR):
-                word2YearVec[word].append(0.0)
-            word2YearVec[word][year] = count
-        totalYearCounts[year] += count
-        wordsInYear[word].add(year)
-
-word2YearVec = dict(word2YearVec)
-
-print('denoising...')
-WINDOW_SIZE = 5;
-half_window = int( WINDOW_SIZE/2)
-for word in word2YearVec:
-    for i in range(len(word2YearVec[word])):
-        if i - half_window > -1 and i+half_window < len(word2YearVec[word]):
-            if sum(word2YearVec[word][i - half_window : i + half_window +1]) == word2YearVec[word][i]:
-                word2YearVec[word][i] = 0
-
-        elif i - WINDOW_SIZE > -1:
-            if sum(word2YearVec[word][i-WINDOW_SIZE:i]) == word2YearVec[word][i]:
-                word2YearVec[word][i] = 0
-
-        elif i+ WINDOW_SIZE <= len(word2YearVec[word]):
-            if sum(word2YearVec[word][i:i+WINDOW_SIZE]) == word2YearVec[word][i]:
-                word2YearVec[word][i] = 0
 
 
-#scale word vectors, divide each word in year, by total occurance count of that word.
-print('tf-idfing...')
-for word, wordVec in word2YearVec.items():
-    yearsWithWord = len(wordsInYear[word])
+def dump_w2yv(word2YearVec, file):
 
-    for year, value in enumerate(wordVec):
-        if value:
+    for i in word2YearVec:
+        file.write(i + "++||++" + str(word2YearVec[i]) + '\n')
+
+def load_w2yv(file):
+    word2YearVec = defaultdict(list)
+
+    for i,l in enumerate(file):
+        #print(l)
+        key = l.split('++||++')[0]
+        vals = []
+        for ent in l.split('++||++')[1][1:-2].split(','):
+            if ent:
+                #print('ent', ent)
+                vals.append(float(ent))
+
+        word2YearVec[key] = vals
+
+    return word2YearVec
+
+
+if __name__ == "__main__":
+
+    LEN_YEAR_VECTOR = 1019
+    year_book = pickle.load(open('../../data_sets/year_book_sample.pickle', 'rb'))
+    word2YearVec = defaultdict(list)            # { word -> LIST(year_i -> REAL) }
+    totalYearCounts = defaultdict(int)          # { year -> int_tot_wrd_count }
+    wordsInYear = defaultdict(set)              # { word -> SET(year0, year1, year2) }
+
+    for year, year_page in enumerate(year_book):
+        for word, count in year_page.items():
             try:
-                word2YearVec[word][year] = (value / totalYearCounts[year]) \
-                                        * log(LEN_YEAR_VECTOR / yearsWithWord)
+                word2YearVec[word][year] = count
+            except IndexError:
+                for i in range(LEN_YEAR_VECTOR):
+                    word2YearVec[word].append(0.0)
+                word2YearVec[word][year] = count
+            totalYearCounts[year] += count
+            wordsInYear[word].add(year)
 
-            except ZeroDivisionError:
-                print(word)
-                print(wordVec)
-                print(yearsWithWord)
-                print(value)
-                print(totalYearCounts[year])
-                print()
+    word2YearVec = dict(word2YearVec)
+
+    print('denoising...')
+    WINDOW_SIZE = 5;
+    half_window = int( WINDOW_SIZE/2)
+    for word in word2YearVec:
+        for i in range(len(word2YearVec[word])):
+            if i - half_window > -1 and i+half_window < len(word2YearVec[word]):
+                if sum(word2YearVec[word][i - half_window : i + half_window +1]) == word2YearVec[word][i]:
+                    word2YearVec[word][i] = 0
+
+            elif i - WINDOW_SIZE > -1:
+                if sum(word2YearVec[word][i-WINDOW_SIZE:i]) == word2YearVec[word][i]:
+                    word2YearVec[word][i] = 0
+
+            elif i+ WINDOW_SIZE <= len(word2YearVec[word]):
+                if sum(word2YearVec[word][i:i+WINDOW_SIZE]) == word2YearVec[word][i]:
+                    word2YearVec[word][i] = 0
+
+
+    #scale word vectors, divide each word in year, by total occurance count of that word.
+    print('tf-idfing...')
+    for word, wordVec in word2YearVec.items():
+        yearsWithWord = len(wordsInYear[word])
+
+        for year, value in enumerate(wordVec):
+            if value:
+                try:
+                    word2YearVec[word][year] = (value / totalYearCounts[year]) \
+                                            * log(LEN_YEAR_VECTOR / yearsWithWord)
+
+                except ZeroDivisionError:
+                    print(word)
+                    print(wordVec)
+                    print(yearsWithWord)
+                    print(value)
+                    print(totalYearCounts[year])
+                    print()
 
 
 
 
-print('pickling...')
-pickle.dump(word2YearVec, open('../../data_sets/w2yv.pickle', 'wb'))
+    print('pickling...')
+    dump_w2yv(word2YearVec, open('temp', 'w+'))
+    #pickle.dump(word2YearVec, open('../../data_sets/w2yv.pickle', 'wb'))
--- preprocess/model_factory/Dataset_2_Pickle.py
+++ preprocess/model_factory/Dataset_2_Pickle.py
@@ -17,7 +17,28 @@ class NoDateFoundException(Exception):
 #gets a string and returns the average of the years that appear in that string
 
 
+def dump_w2yv(year_book, file):
+    file.write(str(len(year_book)) + '\n')
 
+    for i in year_book:
+        file.write(str(i) + '\n')
+
+def load_w2yv(file):
+    year_book = []
+    for i in range(int(file.readline())):
+        year_book.append(defaultdict(int))
+
+    for i,l in enumerate(file):
+        print(l)
+        for ent in l[1:-2].split(','):
+            if ent:
+                year_book[i][ent.split("'")[1]] = int(ent.split(':')[1])
+
+
+
+
+
+    return year_book
 
 def duplicate_punctuation(paragraph):
     puncs = ['–', '-', '/', ',', '.']
@@ -82,61 +103,65 @@ def get_init_date(paragraphs):
 
         return res
 
-wiki = json.loads(open('../../data_sets/wiki_lookup_sample.json').read())
-year_book = [] #dictionary of years and the document that belongs to that year
 
-for i in range(1019):
-    year_book.append(defaultdict(int))
+if __name__ == "__main__":
 
+    wiki = json.loads(open('../../data_sets/wiki_lookup_sample.json').read())
+    year_book = [] #dictionary of years and the document that belongs to that year
 
-#pprint.pprint(wiki['Wham-O'])
-topics = list(wiki.keys())
-#random.shuffle(topics)
-i = 0;
-cleaner = Cleaner()
-
-for topic in topics:
-    i += 1
-    if i%100 == 0:
-        print('\r'+str(i), end='')
-    paragraphs = wiki[topic]['text'].split('\n')
-    try:
-        prev_date = get_init_date(paragraphs)
-    except NoDateFoundException:
-        continue
-
-    para = cleaner.clean(paragraphs[1])
-
-    for year in prev_date:
-        for word in para:
-            year_book[year-1000][word] += 1
-    #year_book[prev_date-1000].append(paragraphs[1])
+    for i in range(1019):
+        year_book.append(defaultdict(int))
 
 
+    #pprint.pprint(wiki['Wham-O'])
+    topics = list(wiki.keys())
+    #random.shuffle(topics)
+    i = 0;
+    cleaner = Cleaner()
+
+    for topic in topics:
+        i += 1
+        if i%100 == 0:
+            print('\r'+str(i), end='')
+        paragraphs = wiki[topic]['text'].split('\n')
+        try:
+            prev_date = get_init_date(paragraphs)
+        except NoDateFoundException:
+            continue
+
+        para = cleaner.clean(paragraphs[1])
+
+        for year in prev_date:
+            for word in para:
+                year_book[year-1000][word] += 1
+        #year_book[prev_date-1000].append(paragraphs[1])
 
 
-    for paragraph in paragraphs[2:]:
-        if len(paragraph) > 50:
-            date = get_date(paragraph)
-
-            if not date:
-                date = prev_date
-
-            #print('\n\n')
-            #print('p: ', paragraph)
-            #print('date: ', date)
-            #print('\n\n')
-
-            prev_date = date
-
-            #add to data structure
 
 
-            paragraph = cleaner.clean(paragraph)
+        for paragraph in paragraphs[2:]:
+            if len(paragraph) > 50:
+                date = get_date(paragraph)
 
-            for year in date:
-                for word in paragraph:
-                    year_book[year-1000][word] += 1
+                if not date:
+                    date = prev_date
+
+                #print('\n\n')
+                #print('p: ', paragraph)
+                #print('date: ', date)
+                #print('\n\n')
+
+                prev_date = date
+
+                #add to data structure
 
 
-pickle.dump(year_book, open('../../data_sets/year_book_sample.pickle', 'wb'))
+                paragraph = cleaner.clean(paragraph)
+
+                for year in date:
+                    for word in paragraph:
+                        year_book[year-1000][word] += 1
+
+
+    dump_w2yv(year_book, open("../../data_sets/year_book_sample.txt", 'wb'))
+    #pickle.dump(year_book, open('../../data_sets/year_book.pickle', 'wb'))
