a9ed96e4e470315c50c6363cf231b886ad7c4c89
Julian Vanecek <julian.vanecek@gmail.com>
2018-12-07 15:47:48 -0500

    FIXED_DATASET_ALMOST_WORKING_JUST_NEED_TO_FIX_DIMENSIONALITY

--- preprocess/lstm/RUN_LSTM_TRAINING.py
+++ preprocess/lstm/RUN_LSTM_TRAINING.py
@@ -7,6 +7,9 @@ import torch.nn as nn
 import torch.optim as optim
 from matplotlib import pyplot as plt
 import time
+import torch.utils.data as td
+import torch
+import numpy as np
 from torch.autograd import Variable
 import sys
 sys.path.append('../model_factory')
@@ -16,11 +19,44 @@ from torch.utils.data import DataLoader
 
 DEFAULT_Q_FILE_PATH = '../../../../qanta-codalab/data/qanta.train.2018.04.18.json'
 DEFAULT_Q_YEAR_PATH = '../../data_sets/wiki_article_to_year.pickle'
-DEFAULT_W2YV_PATH   = '../../data_sets/w2yv.pickle'
+DEFAULT_W2YVD_PATH   = '../../data_sets/w2yv_dic.pickle'
+DEFAULT_W2YVV_PATH   = '../../data_sets/w2yv_vals.npy'
+
 DEFAULT_V_FILE_PATH = '../../../../qanta-codalab/data/qanta.test.2018.04.18.json'
-BATCH_SIZE = 32
+BATCH_SIZE = 1
+MAX_LENGTH =150
+EMBEDDING_DIM=1019
+DEFAULT_YEAR_VEC= [0.0]*EMBEDDING_DIM
 
 
+class YVDataset(td.Dataset):
+  def __init__(self, file_path, wiki_year_dict, w2yv_dict, w2yv_vals):
+    self.w2yvVals = w2yv_vals
+    cleaner = TokenCleaner.Cleaner()
+    print('Loading dataset from ', file_path)
+    self.data_x, self.data_y = [], []
+    with open(file_path,'r') as F:
+        for thing in F:
+            j = json.loads(thing)['questions']
+            for question_chunk in j:
+                wiki_page = question_chunk['page']
+                if wiki_page in wiki_year_dict:
+                    wiki_year = wiki_year_dict[wiki_page]
+                    question_words = cleaner.clean(question_chunk['text'])
+                    question_words = [w2yv_dict[word] for word in question_words if word in w2yv_dict]
+                    sent_len = len(question_words)
+                    question = question_words+([-1]*(MAX_LENGTH - sent_len)) if sent_len < MAX_LENGTH else question_words[-sent_len:] #PAD or CONCAT
+                    self.data_x.append(question)
+                    self.data_y.append(wiki_year)
+
+  def __getitem__(self, index):
+    sentence = self.data_x[index]
+    features = torch.FloatTensor([self.w2yvVals[word] if word != -1 else DEFAULT_YEAR_VEC for word in sentence])
+    labels = torch.LongTensor(np.array(self.data_y[index]))
+    return (features, labels)
+
+  def __len__(self):
+    return len(self.data_x)
 
 class LSTM_Loader:
     def TIME(self):
@@ -28,22 +64,24 @@ class LSTM_Loader:
         self.sT=time.time()
         return s
 
-    def __init__(self, name, word2yearvec_path=DEFAULT_W2YV_PATH, question_file_path=DEFAULT_Q_FILE_PATH, validate_file_path=DEFAULT_V_FILE_PATH, question_year_path=DEFAULT_Q_YEAR_PATH):
+    def __init__(self, name, word2yeardic_path=DEFAULT_W2YVD_PATH, word2yearval_path=DEFAULT_W2YVV_PATH, question_file_path=DEFAULT_Q_FILE_PATH, validate_file_path=DEFAULT_V_FILE_PATH, question_year_path=DEFAULT_Q_YEAR_PATH):
         self.sT = time.time()
         self.name = str(name)
         self.q_file_path = question_file_path
         self.v_file_path = validate_file_path
         self.q_year_path = question_year_path
-        self.w2yv_path = word2yearvec_path
+        self.w2yvD_path = word2yeardic_path
+        self.w2yvV_path = word2yearval_path
         self.wiki_year_dict = pickle.load(open(self.q_year_path, 'rb'))
 
         if os.path.isfile('../../models/'+name):
             self.lstm = torch.load('../../models/'+name)
         else:
             print('loading year dict', self.TIME())
-            self.w2yv_dict = pickle.load(open(self.w2yv_path, 'rb'))
+            self.w2yv_dict = pickle.load(open(self.w2yvD_path, 'rb'))
+            self.w2yv_vals = np.load(open(self.w2yvV_path, 'rb'))
             print('initializing model', self.TIME())
-            self.lstm           = lstm_model.YearLSTM(w2yv_dict, BATCH_SIZE )
+            self.lstm           = lstm_model.YearLSTM(EMBEDDING_DIM, BATCH_SIZE )
             self.loss_function  = nn.NLLLoss()
             self.optimizer      = optim.SGD(self.lstm.parameters(), lr=0.003, nesterov=True, momentum=0.9)
             print('preparing train structures', self.TIME())
@@ -77,11 +115,11 @@ class LSTM_Loader:
         for epoch in range(num_epochs):
             epoch_correct, epoch_loss, valid_correct, valid_loss = 0.0, 0.0, 0.0, 0.0
             for iii, (sentence, tag) in enumerate(training_data):
-                print(str(iii), str(len_data), '\r',end='')
+
                 tag = tag-1000
                 self.lstm.zero_grad()
                 self.lstm.hidden = self.lstm.init_hidden()
-                target = Variable(torch.LongTensor([tag]))
+                target = Variable(tag)
                 pred_year = self.lstm(sentence)
                 loss = self.loss_function(pred_year, target)
                 loss.backward()
@@ -97,7 +135,7 @@ class LSTM_Loader:
                     for iii, (sentence, tag) in enumerate(validation):
                         tag = tag-1000
                         self.lstm.hidden = self.lstm.init_hidden()
-                        target = Variable(torch.LongTensor([tag]))
+                        target = Variable(tag)
                         pred_year = self.lstm(sentence)
                         vloss = self.loss_function(pred_year, target)
                         valid_loss += vloss
@@ -109,31 +147,11 @@ class LSTM_Loader:
         return (train_accuracy, train_loss, test_accuracy, test_loss)
 
     def train(self):
-        cleaner = TokenCleaner.Cleaner()
-        training_set, validation_set = [], []
-        with open(self.q_file_path,'r') as F:
-            for thing in F:
-                j = json.loads(thing)['questions']
-                for question_chunk in j:
-                    wiki_page = question_chunk['page']
-                    if wiki_page in self.wiki_year_dict:
-                        wiki_year = self.wiki_year_dict[wiki_page]
-                        question_words = cleaner.clean(question_chunk['text'])
-                        question = [self.w2yv_dict[word] for word in question_words if word in self.w2yv_dict]
-                        training_set.append((question, wiki_year))
-        with open(self.v_file_path,'r') as F:
-            for thing in F:
-                j = json.loads(thing)['questions']
-                for question_chunk in j:
-                    wiki_page = question_chunk['page']
-                    if wiki_page in self.wiki_year_dict:
-                        wiki_year = self.wiki_year_dict[wiki_page]
-                        question_words = cleaner.clean(question_chunk['text'])
-                        question = [self.w2yv_dict[word] for word in question_words if word in self.w2yv_dict]
-                        validation_set.append((question, wiki_year))
+        training_set, validation_set = YVDataset(self.q_file_path, self.wiki_year_dict, self.w2yv_dict, self.w2yv_vals), YVDataset(self.v_file_path, self.wiki_year_dict, self.w2yv_dict, self.w2yv_vals)
 
         trainloader = DataLoader(training_set, batch_size=BATCH_SIZE, shuffle=True)
         validloader = DataLoader(validation_set, batch_size=BATCH_SIZE, shuffle=True)
+
         results = self._train_all_epochs_(trainloader,validloader)
         self.save_data_model(results, self.lstm)
 
